2e2a3563248bbe5eebae0bbd53ce7562e6597f79
avramaral <avramaral@gmail.com>
2020-02-29 03:28:53 -0300

    update index page

--- Código/Gráficos.R
+++ Código/Gráficos.R
@@ -57,3 +57,17 @@ plot(dados$x, dados$y,
 # Regressão linear
 model = lm(dados$y ~ dados$x)
 abline(model, col = 'red', lwd = 3)
+
+# Boxplot
+dados = data.frame(
+  idade = c(31, 32, 32, 32, 33, 35, 36, 36, 37, 37, 38, 39, 39, 40, 40, 40, 41, 42, 42, 43, 
+            43, 44, 45, 45, 46, 46, 47, 48, 48, 51, 53, 55, 56, 56, 60, 60, 61, 62, 76, 79,
+            21, 24, 25, 26, 26, 26, 26, 27, 28, 30, 30, 31, 31, 33, 33, 33, 34, 34, 34, 34,
+            35, 35, 35, 37, 37, 38, 39, 41, 41, 41, 42, 44, 49, 50, 60, 61, 61, 74, 80, 81),
+  sexo  = rep(c('Homem', 'Mulher'), each = 40)
+)
+boxplot(dados$idade ~ dados$sexo,
+        ylim = c(15, 85),
+        main = 'Boxplot para comparação de idades',
+        xlab = '', ylab = 'Idade'
+)
